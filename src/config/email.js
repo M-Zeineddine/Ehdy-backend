a05d59fce5cba1015e690854b7b2fa511f91bb1d
M-Zeineddine <102983299+M-Zeineddine@users.noreply.github.com>
2026-03-12 00:44:55 +0200

    Remove SENDGRID_API_KEY production requirement

--- src/config/email.js
+++ src/config/email.js
@@ -1,9 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 
-if (!process.env.SENDGRID_API_KEY && process.env.NODE_ENV === 'production') {
-  throw new Error('SENDGRID_API_KEY environment variable is required in production');
-}
 
 if (process.env.SENDGRID_API_KEY) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
